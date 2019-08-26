d87c2b98658d15d3642a8c2d568d171584f406ca
Rai Icasiano <ricasiano@gmail.com>
2019-08-26 21:37:19 +0800

    implement business rules for cash out legal person

--- spec/Transactions/Commissions/CashOut/LegalPersonSpec.php
+++ spec/Transactions/Commissions/CashOut/LegalPersonSpec.php
@@ -1,18 +1,18 @@
 <?php
 
-namespace spec\CommissionCalculator\Transactions\Commissions\CashIn;
+namespace spec\CommissionCalculator\Transactions\Commissions\CashOut;
 
-use CommissionCalculator\Transactions\Commissions\CashIn\DefaultCommission;
+use CommissionCalculator\Transactions\Commissions\CashOut\LegalPerson;
 use CommissionCalculator\Transactions\CurrencyConverter;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
 
 /**
- * @mixin DefaultCommission
+ * @mixin LegalPerson
  */
-class DefaultCommissionSpec extends ObjectBehavior
+class LegalPersonSpec extends ObjectBehavior
 {
-    function it_should_return_the_correct_amount_if_converted_to_value_is_less_than_five_euros(
+    function it_should_return_the_correct_amount_if_converted_to_value_is_more_than_five_cents(
         CurrencyConverter $currencyConverter
     )
     {
@@ -24,15 +24,15 @@ class DefaultCommissionSpec extends ObjectBehavior
         $this->computeCommission()->shouldBeApproximately(4.5, 1.0e-9);
     }
 
-    function it_should_return_the_correct_amount_if_converted_to_value_is_more_than_five_euros(
+    function it_should_return_5_cents_if_computed_amount_is_less_than_5_cents(
         CurrencyConverter $currencyConverter
     )
     {
         $currencyConverter->beADoubleOf(CurrencyConverter::class);
-        $currencyConverter->computeConvertedAmount()->willReturn(1000);
+        $currencyConverter->computeConvertedAmount()->willReturn(1);
 
         $this->beConstructedWith($currencyConverter);
 
-        $this->computeCommission()->shouldBe(5.00);
+        $this->computeCommission()->shouldBe(0.05);
     }
 }
